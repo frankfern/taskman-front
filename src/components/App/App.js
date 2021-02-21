8d719e6a00af3546b4eec6584da48359d45d74e7
Frank Fernandez <frankfernogando@gmail.com>
2021-02-20 23:21:30 -0500

    adding structure

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+
+
+
+
+export default function App(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                {/* <Route path="/" component=""/> */}
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+import App from './components/APP';
+
+
+
+const container = element.getElementById('root');
+
+
+ReactDOM.render(<App />,container);
+
+
